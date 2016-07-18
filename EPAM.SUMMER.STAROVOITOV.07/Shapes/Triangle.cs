33c3f0463e8bc82723439904b3bdf1691bddbeb0
VladislavStarovoitov <VladislavStarovoitov>
2016-07-18 21:16:56 +0300

    ...

--- EPAM.SUMMER.STAROVOITOV.07/Shapes/Triangle.cs
+++ EPAM.SUMMER.STAROVOITOV.07/Shapes/Triangle.cs
@@ -8,6 +8,59 @@ namespace Shapes
 {
     public class Triangle : Shape
     {
+        private double _topAngle;
+        private double _leftAngle;
+        private double _rightAngle;
+        private double _leftSide;
+        private double _rightSide;
+        private double _bottomSide;
+
+        public double TopAngle
+        {
+            get
+            {
+                return _topAngle;
+            }
+            set
+            {
+                if (value > 0 && value < 180)
+                {
+                    _topAngle = value;
+                }
+            }
+        }
+
+        public double LeftSide
+        {
+            get
+            {
+                return _leftSide;
+            }
+            set
+            {
+                if (value > 0)
+                    _leftSide = value;
+            }
+        }
+
+        public double RightSide
+        {
+            get
+            {
+                return _rightSide;
+            }
+            set
+            {
+                if (value > 0)
+                    _rightSide = value;
+            }
+        }
+
+        public Triangle()
+        {
+
+        }
+
         public override double GetArea()
         {
             throw new NotImplementedException();
